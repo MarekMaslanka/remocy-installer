21b867eb169d33e3abe376047c90d17cc3053260
winte <winte@DESKTOP-FCC21E3>
2020-09-26 23:00:44 +0200

    Init

---
+++ make_installer.bat
@@ -0,0 +1,2 @@
+C:\\Users\\winte\\Downloads\\wix311-binaries\\candle.exe -arch x64 lunremote.wxs Custom_InstallDir.wxs Custom_InstallDirDlg.wxs
+C:\\Users\\winte\\Downloads\\wix311-binaries\\light.exe -ext WixUIExtension lunremote.wixobj Custom_InstallDir.wixobj Custom_InstallDirDlg.wixobj -out lunremote.msi
