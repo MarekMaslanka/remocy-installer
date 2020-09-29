fe2718e6083dc44d5f672e6f68fd3d6c23b6eb2b
winte <winte@DESKTOP-FCC21E3>
2020-09-29 17:19:07 +0200

    Windows: Add service and firewall rule on install. Add dialog images. Minor changes.

--- make_installer.bat
+++ make_installer.bat
@@ -1,2 +1,2 @@
-C:\\Users\\winte\\Downloads\\wix311-binaries\\candle.exe -arch x64 lunremote.wxs Custom_InstallDir.wxs Custom_InstallDirDlg.wxs
-C:\\Users\\winte\\Downloads\\wix311-binaries\\light.exe -ext WixUIExtension lunremote.wixobj Custom_InstallDir.wixobj Custom_InstallDirDlg.wixobj -out lunremote.msi
+C:\\Users\\winte\\Downloads\\wix311-binaries\\candle.exe -arch x64 -ext WixUtilExtension -ext WixFirewallExtension lunremote.wxs Custom_InstallDir.wxs Custom_InstallDirDlg.wxs
+C:\\Users\\winte\\Downloads\\wix311-binaries\\light.exe -ext WixUIExtension -ext WixUtilExtension -ext WixFirewallExtension lunremote.wixobj Custom_InstallDir.wixobj Custom_InstallDirDlg.wixobj -out lunremote.msi
